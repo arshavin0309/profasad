9c4fd6a659c983dd2d5507211106b14b7e979c5b
Иванов Владислав <arshavin0309@mail.ru>
2024-03-16 12:34:34 +0500

    add

--- js/index.js
+++ js/index.js
@@ -38,3 +38,39 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 })
+document.addEventListener('DOMContentLoaded', () => {
+  const swiperTeam = new Swiper('.swiper-team', {
+    loop: true,
+
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+      },
+      425: {
+        slidesPerView: 2,
+        spaceBetween: 30
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 30
+      },
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 30
+      }
+    }
+  });
+})
